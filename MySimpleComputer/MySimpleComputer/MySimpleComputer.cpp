0af0838d99181c8b2de7c137ffdb74a6559ed1be
FreDriCk <ferdinonand@gmail.com>
2024-04-07 02:29:58 +0700

    2:35 Commit -> First step to implement CPU commands

--- MySimpleComputer/MySimpleComputer/MySimpleComputer.cpp
+++ MySimpleComputer/MySimpleComputer/MySimpleComputer.cpp
@@ -12,17 +12,26 @@ static int _memory[_memorySize];
 
 
 // Регистр флагов содержит 5 разрядов:
-// 1) Переполнение при выполнении операции;		[CF] - Carry flag
-// 2) Ошибка деления на 0;						[ZF] - Zero flag
-// 3) Ошибка выхода за границы памяти;			[OF] - Overflow flag
-// 4) Игнорирование тактовых импульсов;			[IF] - Interrupt enable flag
-// 5) Указана неверная команда.					[MF] - Method failure flag
+// 1) Переполнение при выполнении операции;		[П]			[CF] - Carry flag
+// 2) Ошибка деления на 0;						[О]			[ZF] - Zero flag
+// 3) Ошибка выхода за границы памяти;			[М]			[OF] - Overflow flag
+// 4) Игнорирование тактовых импульсов;			[Т]			[IF] - Interrupt enable flag
+// 5) Указана неверная команда.					[Е]			[MF] - Method failure flag
 int _regFlag;
-#define CF 1
-#define ZF 2
-#define OF 3
-#define IF 4
-#define MF 5
+
+
+/// <summary>
+/// Аккумулятор, используется для временного хранения данных и результатов операции
+/// </summary>
+int _accumulator;
+
+
+/// <summary>
+/// Счётчик комманд, указывающий на адрес ячейки памяти, в которой хранится текущая выполняемая операция
+/// </summary>
+int _instructionCounter;
+
+
 
 
 
@@ -55,6 +64,7 @@ int sc_memoryInit() {
 /// <returns>
 /// <para/> 1: Успех;
 /// <para/> -1: Ошибка, выход за допустимые границы;
+/// <para/> -2: Значение вышло за границы памяти (более 14 разрядов);
 /// </returns>
 int sc_memorySet(int address, int value) {
 
@@ -63,7 +73,22 @@ int sc_memorySet(int address, int value) {
 		return -1;
 	}
 
-	_memory[address] = value;
+	// В память можно записать значение не превышающее 14 разрядов
+	if (std::abs(value) >= (1 << 13)) {
+		_regFlag = CF;
+		return -2;
+	}
+
+	// Если число отрицательное, в старший разряд (14 разряд) записывается '1'
+	if (value < 0) {
+		int absValue = (~value) + 1;
+		value = absValue | (1 << 13);
+	}
+
+	// Для отличия команды от значения необходимо записать в 15 разряд числа значение 1
+	int newValue = value | (1 << 14);
+
+	_memory[address] = newValue;
 
 	return 1;
 }
@@ -79,6 +104,7 @@ int sc_memorySet(int address, int value) {
 /// <returns>
 /// <para/> 1: Успех;
 /// <para/> -1: Ошибка, выход за допустимые границы;
+/// <para/> -2: Попытка достать команду;
 /// </returns>
 int sc_memoryGet(int address, int* value) {
 
@@ -87,7 +113,25 @@ int sc_memoryGet(int address, int* value) {
 		return -1;
 	}
 
-	*value = _memory[address];
+	int recValue = _memory[address];
+
+	// Проверка 15 разряда, вдруг пытаются достучаться до команды
+	bool isNumber = (bool((1 << 14) & recValue));
+	if (!isNumber) {
+		return -2;
+	}
+
+	// Записываем '0' в 15 разряд
+	int clearValue = recValue & ~(1 << 14);
+
+	// Если 14 разряд равен 1, то число является отрицательным
+	// В таком случае зануляем 14 разряд и умножаем на -1
+	bool isNegative = (bool((1 << 13) & clearValue));
+	if (isNegative) {
+		clearValue = (clearValue & ~(1 << 13)) * -1;
+	}
+
+	*value = clearValue;
 
 	return 1;
 }
@@ -253,12 +297,53 @@ int sc_regGet(int reg, int* value) {
 /// <param name="value">Значение</param>
 /// <returns>
 /// <para/> 1: Успех;
-/// <para/> -1: Ошибка;
+/// <para/> -1: Команда не поддерживается ЦП;
+/// <para/> -2: Операнд вышел за границы памяти (максимум 7 разрядов);
+/// <para/> -3: В оперативной памяти не осталось свободного места;
 /// </returns>
 int sc_commandEncode(int command, int operand, int* value) {
 
+	if (!commandExists(command)) {
+		//_regFlag = MF;
+		return -1;
+	}
 
-	return -1;
+	// Для операнда предназначены 7 разрядов
+	// Проверка на вместимость значения в память
+	if (std::abs(operand) >= (1 << 6)) {
+		//_regFlag = CF;
+		return -2;
+	}
+
+	// Если число отрицательное, в старший разряд (7 разряд) записывается '1'
+	if (operand < 0) {
+		int absValue = (~operand) + 1;
+		operand = absValue | (1 << 6);
+	}
+
+	// Для кода операции выделены 7 разрядов в середине (14 - 7)
+	int subCommand = command << 7;
+
+	int result = subCommand | operand;
+
+	// Поиск места в оперативной памяти, куда можно записать команду
+	int commandIndex = -1;
+	for (int i = 0; i < _memorySize; ++i) {
+		if (_memory[i] == 0) {
+			commandIndex = i;
+			break;
+		}
+	}
+
+	if (commandIndex == -1) {
+		return -3;
+	}
+
+	_memory[commandIndex] = result;
+	
+	*value = result;
+
+	return 1;
 }
 
 
@@ -271,10 +356,178 @@ int sc_commandEncode(int command, int operand, int* value) {
 /// <param name="operand">Операнд</param>
 /// <returns>
 /// <para/> 1: Успех;
-/// <para/> -1: Ошибка;
+/// <para/> -1: Не удалось найти команду;
+/// <para/> -2: Попытка декодировать число;
 /// </returns>
 int sc_commandDecode(int value, int* command, int* operand) {
 
+	// Поиск индекса в массиве, где расположена команда
+	int commandIndex = -1;
+	for (int i = 0; i < _memorySize; ++i) {
+		if (_memory[i] == value) {
+			commandIndex = i;
+			break;
+		}
+	}
+
+	if (commandIndex == -1) {
+		return -1;
+	}
+
+	int recCommand = _memory[commandIndex];
+
+	// Проверка 15 разряда, вдруг пытаются достучаться до числа
+	bool isNumber = (bool((1 << 14) & recCommand));
+	if (isNumber) {
+		return -2;
+	}
+
+	// Инициализация индекса команды
+	int subCommand = recCommand >> 7;
+
+	// Зануление индексов команды для определения операнда
+	int subOperand = recCommand;
+	for (int i = 13; i > 6; --i) {
+		subOperand = subOperand & ~(1 << i);
+	}
+
+	// Если 7 разряд равен 1, то число является отрицательным
+	// В таком случае зануляем 7 разряд и умножаем на -1
+	bool isNegative = (bool((1 << 6) & subOperand));
+	if (isNegative) {
+		subOperand = (subOperand & ~(1 << 6)) * -1;
+	}
+
+	*command = subCommand;
+	*operand = subOperand;
+
+	_instructionCounter = commandIndex;
+
+	executeCommand(subCommand, subOperand);
+
+	// TODO: Когда очищать команды????
+
+	return 1;
+}
+
+
+/// <summary>
+/// Получить значение счётчика команд
+/// </summary>
+/// <returns></returns>
+int sc_instructGet() {
+	return _instructionCounter;
+}
+
+
+/// <summary>
+/// Установить значение счётчика команд
+/// </summary>
+/// <param name="value">Новое значение</param>
+void sc_instructSet(int value) {
+	_instructionCounter = value;
+}
+
+
+/// <summary>
+/// Получить значение аккумулятора
+/// </summary>
+/// <returns></returns>
+int sc_accumGet() {
+	return _accumulator;
+}
+
+
+/// <summary>
+/// Установить значение аккумулятора
+/// </summary>
+/// <param name="value"></param>
+void sc_accumSet(int value) {
+	_accumulator = value;
+}
+
+
+/// <summary>
+/// Проверка на наличие команды процессора
+/// </summary>
+/// <param name="command">Номер команды</param>
+/// <returns>
+/// <para/> true: Успех;
+/// <para/> false: Ошибка;
+/// </returns>
+bool commandExists(int command) {
+
+	// Операции ввода\вывода
+	if (command == 10 || command == 11)
+		return true;
+
+	// Операции загрузки\выгрузки в аккумулятор
+	if (command == 20 || command == 21)
+		return true;
+
+	// Арифметические операции
+	if (command >= 30 || command <= 33)
+		return true;
+
+	// Операции передачи управления
+	if (command >= 40 || command <= 43)
+		return true;
+
+	// Пользовательские функции
+	if (command >= 51 || command <= 76)
+		return true;
+
+	return false;
+}
+
+
+/// <summary>
+/// Выполнить команду ЦП
+/// </summary>
+/// <param name="command">Номер команды</param>
+/// <param name="operand">Операнд</param>
+/// <returns>
+/// <para/> 1: Успех;
+/// <para/> -1: Команда не реализована;
+/// </returns>
+int executeCommand(int command, int operand) {
+
+	int clearValue, idCom, idOper;
+	switch (command)
+	{
+		case LOAD:
+			// Записываем '0' в 15 разряд
+			clearValue = _memory[operand] & ~(1 << 14);
+			_accumulator = clearValue;
+			return 1;
+
+		case SUB:
+			// Записываем '0' в 15 разряд
+			clearValue = _memory[operand] & ~(1 << 14);
+			_accumulator -= clearValue;
+			return 1;
+
+		case JNEG:
+			if (_accumulator < 0) {
+				// TODO: Разобраться в переходах по программе
+				// Возможно придётся переосмыслить работу с _instructionCounter
+				int recCommand = _memory[operand];
+				sc_commandDecode(recCommand, &idCom, &idOper);
+			}
+			return 1;
+
+		case WRITE:
+			// Записываем '0' в 15 разряд
+			clearValue = _memory[operand] & ~(1 << 14);
+			std::cout << clearValue;
+			return 1;
+
+		case HALT:
+			// Как сбросить программу???
+			sc_regInit();
+			sc_memoryInit();
+			return 1;
+	}
 
 	return -1;
 }
--- MySimpleComputer/MySimpleComputerTests/MySimpleComputerTests.cpp
+++ MySimpleComputer/MySimpleComputerTests/MySimpleComputerTests.cpp
@@ -32,12 +32,12 @@ namespace MySimpleComputerTests
 		{
 			sc_memoryInit();
 
-			sc_memorySet(1, 25);
+			sc_memorySet(1, -10);
 
 			int value;
 			int memGetGood = sc_memoryGet(1, &value);
 			Assert::AreEqual(memGetGood, 1);
-			Assert::AreEqual(value, 25);
+			Assert::AreEqual(value, -10);
 
 			// Симуляция изменения значения value
 			value = 123;
@@ -114,7 +114,7 @@ namespace MySimpleComputerTests
 		/// </summary>
 		TEST_METHOD(TestFlagGet)
 		{
-			int flagInit = sc_regInit();
+			sc_regInit();
 
 			sc_regSet(1, 1);
 			
@@ -122,6 +122,133 @@ namespace MySimpleComputerTests
 			int regGetGood = sc_regGet(1, &value);
 			Assert::AreEqual(regGetGood, 1);
 			Assert::AreEqual(value, 1);
+		}
+
+
+		/// <summary>
+		/// Тест записи метода в ЦП
+		/// </summary>
+		TEST_METHOD(TestMethodEncode)
+		{
+			sc_memoryInit();
+
+			// 10 = 1010
+			// 20 = 10100
+			// result: 0_0001010_0010100 = 1300
+
+			int commandPtr;
+			int goodEncode = sc_commandEncode(10, 20, &commandPtr);
+			Assert::AreEqual(goodEncode, 1);
+			Assert::AreEqual(commandPtr, 1300);
+
+
+			// 10 = 1010
+			// -20 = 1010100
+			// result: 0_0001010_1010100 = 1364
+
+			sc_commandEncode(10, -20, &commandPtr);
+			Assert::AreEqual(commandPtr, 1364);
+		}
+
+
+		/// <summary>
+		/// Тест чтения метода из ЦП
+		/// </summary>
+		TEST_METHOD(TestMethodDecode)
+		{
+			sc_memoryInit();
+
+			// 10 = 1010
+			// 20 = 10100
+			// result: 0_0001010_0010100 = 1300
+
+			int commandPtr;
+			sc_commandEncode(10, 20, &commandPtr);
+			
+			int commandId;
+			int operand;
+			int goodDecode = sc_commandDecode(commandPtr, &commandId, &operand);
+			Assert::AreEqual(goodDecode, 1);
+			Assert::AreEqual(commandId, 10);
+			Assert::AreEqual(operand, 20);
+
+
+			// 76 = 1001100
+			// -20 = 1010100
+			// result: 0_1001100_1010100 = 9812
+
+			sc_commandEncode(76, -20, &commandPtr);
+
+			sc_commandDecode(commandPtr, &commandId, &operand);
+			Assert::AreEqual(commandId, 76);
+			Assert::AreEqual(operand, -20);
+		}
+
+
+		/// <summary>
+		/// Проверка работы некоторых команд процессора (пример из методички)
+		/// </summary>
+		TEST_METHOD(TestExecution)
+		{
+			sc_regInit();
+			sc_memoryInit();
+
+			// 00: READ A 09		;(Ввод А)
+			sc_memorySet(9, 3);
+
+			// 01: READ B 10		;(Ввод В)
+			sc_memorySet(10, 8190);
+
+			// 02: LOAD 09			;(Загрузка А в аккумулятор)
+			int loadCommand;
+			sc_commandEncode(LOAD, 9, &loadCommand);
+
+			// 03: SUB 10			;(Отнять В)
+			int subCommand;
+			sc_commandEncode(SUB, 10, &subCommand);
+
+			// 04: JNEG 07			;(Переход на 07, если отрицательное)
+			int jnegCommand;
+			sc_commandEncode(JNEG, 7, &jnegCommand);
+
+			// 05: WRITE 09			;(Вывод А)
+			int writeACommand;
+			sc_commandEncode(WRITE, 9, &writeACommand);
+
+			// 06: HALT 00			;(Останов)
+			int firstHaltCommand;
+			sc_commandEncode(HALT, 0, &firstHaltCommand);
+
+			// 07: WRITE 10			;(Вывод В)
+			int writeBCommand;
+			sc_commandEncode(WRITE, 10, &writeBCommand);
+
+			// 08: HALT 00			;(Останов)
+			int secondHaltCommand;
+			sc_commandEncode(HALT, 0, &secondHaltCommand);
+
+
+
+			int command, operand;
+			sc_commandDecode(loadCommand, &command, &operand);
+
+			// Проверка аккумулятора
+			int accum = sc_accumGet();
+			Assert::AreEqual(accum, 3);
+			
+			sc_commandDecode(subCommand, &command, &operand);
+
+			// 3 - 8190 = -8187
+			accum = sc_accumGet();
+			Assert::AreEqual(accum, -8187);
+
+			sc_commandDecode(jnegCommand, &command, &operand);
+
+			sc_commandDecode(writeACommand, &command, &operand);
+
+			sc_commandDecode(writeBCommand, &command, &operand);
+
+			sc_commandDecode(secondHaltCommand, &command, &operand);
 		}
 	};
 }
