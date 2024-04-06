de27dd11bdf00d1dad0909cf1b4f4aa7b32287f2
Alexander Musienko <aalex96@mail.ru>
2024-04-06 15:49:29 +0700

    lab4

---
+++ MyBigChars/MyBigChars/MyBigChars.h
@@ -0,0 +1,6 @@
+#include "../MyTerm/MyTerm.h"
+
+int bc_box(int x1, int y1, int x2, int y2);
+int bc_printbigchar(char key, int x, int y, Colors foreground, Colors background);
+int bc_initMyBigChars();
+int bc_loadSymbols(const char * filename);
